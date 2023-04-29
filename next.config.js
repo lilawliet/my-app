7e40319ca6c3e7493d1113d65a2260626925d6b6
DESKTOP-ST0GILC\w2517 <25175731@qq.com>
2023-04-29 17:30:38 +0800

    next-image-export-optimizer

--- next.config.js
+++ next.config.js
@@ -19,6 +19,27 @@ if (isGithubActions) {
 }
 // console.log(basePath)
 
+const imagesConfig = {
+  images: {
+    loader: 'custom',
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
+  transpilePackages: ['next-image-export-optimizer'],
+  env: {
+    nextImageExportOptimizer_imageFolderPath: 'public',
+    nextImageExportOptimizer_exportFolderPath: 'out',
+    nextImageExportOptimizer_quality: 75,
+    nextImageExportOptimizer_storePicturesInWEBP: true,
+    nextImageExportOptimizer_exportFolderName: 'nextImageExportOptimizer',
+
+    // If you do not want to use blurry placeholder images, then you can set
+    // nextImageExportOptimizer_generateAndUseBlurImages to false and pass
+    // `placeholder="empty"` to all <ExportedImage> components.
+    nextImageExportOptimizer_generateAndUseBlurImages: true,
+  },
+}
+
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['antd-mobile'],
@@ -29,6 +50,7 @@ const nextConfig = {
     unoptimized: true,
   },
   output: 'export',
+  ...imagesConfig,
 }
 
 module.exports = nextConfig
