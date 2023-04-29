f972a7ce3fefeb00ca3695b3ab9e199a2aa2f1fd
DESKTOP-ST0GILC\w2517 <25175731@qq.com>
2023-04-29 19:23:16 +0800

    build only

--- next.config.js
+++ next.config.js
@@ -2,55 +2,26 @@
 
 // 用于为静态资源（如图像、样式表、JavaScript 文件等）设置 URL 前缀
 // 这在将应用部署到自定义域名或 CDN 上时特别有用，因为它允许您将静态资源存储在不同的位置
-let assetPrefix = ``
+// let assetPrefix = ``
 
-// 用于为应用设置基础路径
-// 这在将应用部署到子目录下时特别有用，因为它允许您指定应用所在的目录
-// const basePath = `/`
+// const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
+// if (isGithubActions) {
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
-if (isGithubActions) {
-  // 去掉 `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  // basePath = `/${repo}`
-}
-// console.log(basePath)
-
-const imagesConfig = {
-  images: {
-    loader: 'custom',
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-  },
-  transpilePackages: ['next-image-export-optimizer'],
-  env: {
-    nextImageExportOptimizer_imageFolderPath: 'public/images',
-    nextImageExportOptimizer_exportFolderPath: 'out',
-    nextImageExportOptimizer_quality: 75,
-    nextImageExportOptimizer_storePicturesInWEBP: true,
-    nextImageExportOptimizer_exportFolderName: 'nextImageExportOptimizer',
-
-    // If you do not want to use blurry placeholder images, then you can set
-    // nextImageExportOptimizer_generateAndUseBlurImages to false and pass
-    // `placeholder="empty"` to all <ExportedImage> components.
-    nextImageExportOptimizer_generateAndUseBlurImages: true,
-  },
-}
+//   assetPrefix = `/${repo}/`
+// }
 
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['antd-mobile'],
-  assetPrefix,
+  // assetPrefix,
   // basePath,
   reactStrictMode: true,
   images: {
     unoptimized: true,
+    // loader: 'akamai',
   },
-  output: 'export',
-  ...imagesConfig,
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { Inter } from 'next/font/google'
-import ExportedImage from 'next-image-export-optimizer'
+import Image from 'next/image'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,8 +21,8 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             By{' '}
-            <ExportedImage
-              src="/images/vercel.svg"
+            <Image
+              src="/vercel.svg"
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
@@ -34,9 +34,9 @@ export default function Home() {
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <ExportedImage
+        <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/images/next.svg"
+          src="/next.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
