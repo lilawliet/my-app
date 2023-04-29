de4952ad140088e346b9829c693a020a3c23500e
DESKTOP-ST0GILC\w2517 <25175731@qq.com>
2023-04-30 01:17:48 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 // 用于为静态资源（如图像、样式表、JavaScript 文件等）设置 URL 前缀
 // 这在将应用部署到自定义域名或 CDN 上时特别有用，因为它允许您将静态资源存储在不同的位置
-let assetPrefix = `/`
+let assetPrefix = undefined
 let basePath = ``
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false
