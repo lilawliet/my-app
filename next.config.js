665d9818c7481db71e7870d4709ed74150744240
DESKTOP-ST0GILC\w2517 <25175731@qq.com>
2023-04-30 01:20:37 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ if (isGithubActions) {
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
   assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
+  basePath = `https://lilawliet.github.io/${repo}`
 }
 
 const nextConfig = {
