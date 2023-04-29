3b65b5267745bd88dfe3e7b12956a128329c0025
DESKTOP-ST0GILC\w2517 <25175731@qq.com>
2023-04-30 01:13:37 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 
 // 用于为静态资源（如图像、样式表、JavaScript 文件等）设置 URL 前缀
 // 这在将应用部署到自定义域名或 CDN 上时特别有用，因为它允许您将静态资源存储在不同的位置
-let assetPrefix = ``
+let assetPrefix = `/`
+let basePath = ``
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false
 
@@ -10,12 +11,14 @@ if (isGithubActions) {
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
   assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
 }
 
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['antd-mobile'],
-  assetPrefix: assetPrefix,
+  assetPrefix,
+  basePath,
   reactStrictMode: true,
   images: {
     unoptimized: true,
