4265138c8cea93c368bd7f4d1fcef05eb7229f73
DESKTOP-ST0GILC\w2517 <25175731@qq.com>
2023-05-01 02:04:20 +0800

    ignorecase false

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['antd-mobile'],
   reactStrictMode: true,
-  // basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  // assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   images: {
     unoptimized: true,
   },
