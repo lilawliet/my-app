532b58822b538ddd446b837967fcf818af60b2c1
DESKTOP-ST0GILC\w2517 <25175731@qq.com>
2023-04-30 12:54:26 +0800

    console

--- src/utils/image-loader.js
+++ src/utils/image-loader.js
@@ -2,5 +2,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
 export default function imageLoader({ src, width, quality }) {
-  return isProd ? `/${repo}/${src}?w=${width}&q=${quality || 75}` : src
+  const url = isProd ? `/${repo}/${src}?w=${width}&q=${quality || 75}` : src
+  console.log(url)
+  return url
 }
